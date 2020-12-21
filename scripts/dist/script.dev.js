cb91c43b6e6bec868146ea027da0da2be3b76617
Kirill Strelnikov <vampirick362@gmail.com>
2020-12-22 01:16:29 +0300

    innit

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -1,38 +1,45 @@
 "use strict";
 
-var tab1 = document.getElementById('tab_01');
-var tab2 = document.getElementById('tab_02');
-var tab3 = document.getElementById('tab_03');
-var link1 = document.getElementById('tab-link_01');
-var link2 = document.getElementById('tab-link_02');
-var link3 = document.getElementById('tab-link_03');
-var tabs = [tab1, tab2, tab3];
-var links = [link1, link2, link3];
+document.addEventListener('DOMContentLoaded', function () {
+  //==================TABS========================
+  var tabs = [document.getElementById('tab_01'), document.getElementById('tab_02'), document.getElementById('tab_03')];
+  var links = [document.getElementById('tab-link_01'), document.getElementById('tab-link_02'), document.getElementById('tab-link_03')];
 
-var showTab = function showTab(_ref) {
-  var target = _ref.target;
-  var id = parseInt(target.id[target.id.length - 1]) - 1;
-  if (links[id].classList.contains('disabled')) return;
+  var showTab = function showTab(_ref) {
+    var target = _ref.target;
+    var id = parseInt(target.id[target.id.length - 1]) - 1;
+    if (links[id].classList.contains('disabled')) return null;
+    links.forEach(function (link) {
+      return link.classList.remove('active');
+    });
+    tabs.forEach(function (tab) {
+      return tab.style.display = 'none';
+    });
+    links[id].classList.add('active');
+    tabs[id].style.display = 'block';
+  };
+
   links.forEach(function (link) {
-    return link.classList.remove('active');
-  });
-  links[id].classList.add('active');
-  tabs.forEach(function (tab) {
-    return tab.style.display = 'none';
-  });
-  tabs[id].style.display = 'block';
-};
+    return link.addEventListener('click', showTab);
+  }); //=================MODAL========================
 
-link1.addEventListener('click', showTab);
-link2.addEventListener('click', showTab);
-link3.addEventListener('click', showTab); //=================Modal========================
+  var gameModal = document.getElementById('modal');
+  document.getElementById('open_modal').addEventListener('click', function () {
+    return gameModal.classList.add('open');
+  });
+  document.getElementById('close_modal').addEventListener('click', function () {
+    return gameModal.classList.remove('open');
+  }); //==================SHOW/HIDE ADS===============
 
-var openModal = document.getElementById("open_modal");
-var gameModal = document.getElementById("modal");
-var closeModal = document.getElementById("close_modal");
-openModal.addEventListener('click', function () {
-  return gameModal.classList.add("open");
-});
-closeModal.addEventListener('click', function () {
-  return gameModal.classList.remove("open");
+  var game = document.getElementById('game');
+  var abs = [document.getElementById('ads_l'), document.getElementById('ads_r')]; // const player = document.getElementsByClassName("player")[0]
+
+  var showAds = function showAds() {
+    // 460 это ширина одного рекламного блока с отступами
+    if (game.clientWidth + 460 * 2 > screen.width) abs[0].style.display = 'none';else abs[0].style.display = 'block';
+    if (game.clientWidth + 460 > screen.width) abs[1].style.display = 'none';else abs[1].style.display = 'block'; // if(game.clientWidth >= screen.width) player.style.overscrollBehaviorX="scroll"
+  };
+
+  window.onresize = showAds;
+  showAds();
 });
--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,56 @@
-const tab1 = document.getElementById('tab_01')
-const tab2 = document.getElementById('tab_02')
-const tab3 = document.getElementById('tab_03')
+document.addEventListener('DOMContentLoaded', () => {
+  //==================TABS========================
+  const tabs = [
+    document.getElementById('tab_01'),
+    document.getElementById('tab_02'),
+    document.getElementById('tab_03')
+  ]
+  const links = [
+    document.getElementById('tab-link_01'),
+    document.getElementById('tab-link_02'),
+    document.getElementById('tab-link_03')
+  ]
 
-const link1 = document.getElementById('tab-link_01')
-const link2 = document.getElementById('tab-link_02')
-const link3 = document.getElementById('tab-link_03')
+  const showTab = ({ target }) => {
+    const id = parseInt(target.id[target.id.length - 1]) - 1
 
-const tabs = [tab1, tab2, tab3]
-const links = [link1, link2, link3]
+    if (links[id].classList.contains('disabled')) return null
 
-const showTab = ({ target }) => {
-  const id = parseInt(target.id[target.id.length - 1]) - 1
+    links.forEach((link) => link.classList.remove('active'))
+    tabs.forEach((tab) => (tab.style.display = 'none'))
 
-  if (links[id].classList.contains('disabled')) return;
-  links.forEach((link) => link.classList.remove('active'))
-  links[id].classList.add('active')
+    links[id].classList.add('active')
+    tabs[id].style.display = 'block'
+  }
 
-  tabs.forEach((tab) => (tab.style.display = 'none'))
-  tabs[id].style.display = 'block'
-}
+  links.forEach((link) => link.addEventListener('click', showTab))
 
-link1.addEventListener('click', showTab)
-link2.addEventListener('click', showTab)
-link3.addEventListener('click', showTab)
+  //=================MODAL========================
 
-//=================Modal========================
+  const gameModal = document.getElementById('modal')
 
-const openModal  = document.getElementById("open_modal")
-const gameModal = document.getElementById("modal")
-const closeModal = document.getElementById("close_modal")
+  document
+    .getElementById('open_modal')
+    .addEventListener('click', () => gameModal.classList.add('open'))
+  document
+    .getElementById('close_modal')
+    .addEventListener('click', () => gameModal.classList.remove('open'))
 
-openModal.addEventListener('click', () => gameModal.classList.add("open"))
-closeModal.addEventListener('click', () => gameModal.classList.remove("open"))
+  //==================SHOW/HIDE ADS===============
+
+  const game = document.getElementById('game')
+  const abs = [document.getElementById('ads_l'), document.getElementById('ads_r')]
+  // const player = document.getElementsByClassName("player")[0]
+
+  const showAds = () => {
+    // 460 это ширина одного рекламного блока с отступами
+    if (game.clientWidth + 460 * 2 > screen.width) abs[0].style.display = 'none'
+    else abs[0].style.display = 'block'
+    if (game.clientWidth + 460 > screen.width) abs[1].style.display = 'none'
+    else abs[1].style.display = 'block'
+    // if(game.clientWidth >= screen.width) player.style.overscrollBehaviorX="scroll"
+  }
+
+  window.onresize = showAds
+  showAds()
+})
